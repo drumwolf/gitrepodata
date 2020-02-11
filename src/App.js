6595ec5bcb725b078011f94e31f2301d6b29a829
John Lee <drumwolf@gmail.com>
2020-02-11 15:55:57 -0800

    Reset CSS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header></header>
+        <header className="App-header"></header>
       </div>
     );
   }
