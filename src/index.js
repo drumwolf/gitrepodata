deea227de3ce2d1f286aab6aa05de08448e40305
John Lee <drumwolf@gmail.com>
2020-02-12 21:12:32 -0800

    first iteration of redux, before react-redux

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import store from "./redux/store/index";
+import { addUsername } from "./redux/actions/index";
+window.store = store;
+window.addUsername = addUsername;
+
 ReactDOM.render(<App />, document.getElementById('root'));
