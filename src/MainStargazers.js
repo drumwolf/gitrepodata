4530e8cc776431b6c5dea320fd1b1f97e974ee1c
John Lee <drumwolf@gmail.com>
2020-02-24 11:39:36 -0800

    applied styling to no-stargazers message

--- src/MainStargazers.js
+++ src/MainStargazers.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 
 function MainStargazers({name,data}) {
+  const output = (data.length || !name) ? data : <li className="main-stargazer-item">No stargazers for this repo.</li>;
   return (
     <section className="main-stargazers">
       <h2 className="main-stargazers-header">Stargazers: <span className="main-stargazers-repo">{name}</span></h2>
-      <ul>{data}</ul>
+      <ul>{output}</ul>
     </section>
   );
 }
